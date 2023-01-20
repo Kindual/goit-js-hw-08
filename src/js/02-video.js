92558368717cbf0d76c413c111ff1cc5c4802bd9
Kindual <110603179+Kindual@users.noreply.github.com>
2023-01-20 14:28:06 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,23 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
-console.log(Player)
+const options = {
+    width: 640,
+    loop: true
+};
+
+const player = new Player('vimeo-player', options);
+
+let playerTime = 0;
+
+if (localStorage.getItem("videoplayer-current-time")) {
+    playerTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+    player.setCurrentTime(playerTime)
+}
+
+player.on('timeupdate', throttle(setPlayerSecondsinLS, 1000));
+
+function setPlayerSecondsinLS({seconds}) {
+  playerTime = seconds;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(playerTime));
+}   
