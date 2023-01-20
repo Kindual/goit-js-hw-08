4dfe8e4b16b7e49147a6506312c6c0d34620fe06
Kindual <110603179+Kindual@users.noreply.github.com>
2023-01-20 11:02:51 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(SimpleLightbox);
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML = createElementsGallery(galleryItems);
+gallery.addEventListener('click', openLightBox);
+
+const options = {
+    enableKeyboard: true,
+    captionDelay: 250,
+};
+
+const lightBox = new SimpleLightbox('.gallery__item', options);
+
+function openLightBox(event) {
+    lightBox.overlay = true;
+}
+
+function createElementsGallery(arg) {
+    return arg
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}" title ="${description}" />
+              </a>`;
+        })
+        .join('');
+}
